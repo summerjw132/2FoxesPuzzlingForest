3edea40ea5e81973202b3ad4d5a5543329c9e0a0
Garrison Lee <gmlee970@gmail.com>
2021-01-06 17:40:37 -0700

    Directional Constraints
    Implements directional constraints on walls. Defaults to no constraint. Can be set from the editor, the field is called, "Directional Constraint." Also adds Library to the .gitignore because I've been having issues with it and I think Library just stores local changes

--- Puzzling Forest/Assets/Scripts/Walls/PushableTurnBasedObject.cs
+++ Puzzling Forest/Assets/Scripts/Walls/PushableTurnBasedObject.cs
@@ -6,6 +6,15 @@ public class PushableTurnBasedObject : TurnBasedCharacter
 {
     [SerializeField]
     private bool stackPushingIsEnabled = true;
+    [SerializeField]
+    private DirectionConstraint directionConstraint = DirectionConstraint.None;
+
+    public enum DirectionConstraint
+    {
+        None,
+        Constrained_to_X,
+        Constrained_to_Z
+    }
 
     public override void SpecialAction()
     {
@@ -20,6 +29,34 @@ public class PushableTurnBasedObject : TurnBasedCharacter
     //Given a direction (ideally, 1 unit on the x or z axis), this object is pushed forward in that direction at the given speed
     public bool PushForwardInDirectionOnGridTile(Vector3 direction, float movementSpeed)
     {
+        //only allow push if the block is not constrained against this direction
+        switch (directionConstraint)
+        {
+            //can only move along the x axis
+            case DirectionConstraint.Constrained_to_X:
+                //checks if the attempted direction is along the x axis
+                if (isAlongX(direction))
+                    break;
+                //tried to push block in a direction it is not allowed to move in
+                else
+                {
+                    Debug.Log(this.gameObject.name + " can't be pushed because it is constrained to the X-axis");
+                    return false;
+                }
+            case DirectionConstraint.Constrained_to_Z:
+                if (isAlongZ(direction))
+                    break;
+                else
+                {
+                    Debug.Log(this.gameObject.name + " can't be pushed because it is constrained to the Z-axis");
+                    return false;
+                }
+
+            //not constrained so go ahead
+            default:
+                break;
+        }
+
         Vector3 targetPosition = this.transform.position + direction;
 
         
@@ -36,6 +73,24 @@ public class PushableTurnBasedObject : TurnBasedCharacter
             return false;
         }
         
+    }
+
+    //lil helper fxns
+    public bool isAlongX(Vector3 direction)
+    {
+        direction = direction.normalized;
+        if (direction == Vector3.left || direction == Vector3.right)
+            return true;
+        else
+            return false;
+    }
+
+    public bool isAlongZ(Vector3 direction)
+    {
+        if (direction == Vector3.forward || direction == Vector3.back)
+            return true;
+        else
+            return false;
     }
 
 }
