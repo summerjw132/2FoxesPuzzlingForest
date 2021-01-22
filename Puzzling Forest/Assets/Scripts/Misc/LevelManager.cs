95295b0b6255cf0e8b221c2f1bf4bcd853247dcf
PinedaJeremy <pinedajeremy16@gmail.com>
2021-01-21 19:47:32 -0500

    Added notes to the script to explain what everything is doing

--- Puzzling Forest/Assets/Scripts/Misc/LevelManager.cs
+++ Puzzling Forest/Assets/Scripts/Misc/LevelManager.cs
@@ -17,11 +17,16 @@ public class LevelManager : MonoBehaviour
     {
         PopulateLevels();
     }
-
+    /// <summary>
+    /// This method will populate levels based on the number of SO levels and then instantiate buttons per SO and write all information on the
+    /// buttons to load the proper level.
+    /// </summary>
     void PopulateLevels()
     {
+        //Retrieve the Directory the SO are stored.
         MyLevel[] levels = Resources.LoadAll<MyLevel>("Data/ScriptableObject");
         Button newButton = buttonLevel;
+        //This for loop is iterating through all SO in the Directory above.
         for (int i = 0; i < levels.Length; i++)
         {
             Instantiate(newButton, scrollViewContainer.transform);
@@ -30,6 +35,7 @@ public class LevelManager : MonoBehaviour
             newButton.GetComponent<NextLevel>().nextLevel = levels[i].LevelName;
             allLevels.Add(levels[i]);
         }
+        //This is to get the last level that keeps getting instatiated first removes it from the content viewer and then reparents it so that it will display as the last option available.
         GameObject lastButton = scrollViewContainer.transform.GetChild(0).gameObject;
         lastButton.transform.SetParent(null);
         lastButton.transform.SetParent(scrollViewContainer.transform);
