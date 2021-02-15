90cf0480052c5387c369a008df0c76a390386549
PinedaJeremy <pinedajeremy16@gmail.com>
2021-02-14 23:29:49 -0500

    Audio Manager integrated

---
+++ Puzzling Forest/Assets/Scripts/Misc/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private Slider BackgroundMusicSlider;
+
+    [SerializeField]
+    private AudioSource BackgroundMusicAudio;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BackgroundMusicSlider.value = .7f;
+    }
+
+    public void SetBackgroundMusicValue()
+    {
+        BackgroundMusicAudio.volume = BackgroundMusicSlider.value;
+    }
+}
