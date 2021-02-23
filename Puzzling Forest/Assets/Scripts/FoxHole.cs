8eef216c125d342e801b1802dc30adf8438bc26e
Garrison Lee <gmlee970@gmail.com>
2021-02-23 13:36:31 -0700

    Clean up FoxHoles
     - No longer counts as a floor
     - Will no longer warp a fox to itself if it is covered by an object
     - Displays a UI msg if the warp is blocked by a covering

--- Puzzling Forest/Assets/Scripts/FoxHole.cs
+++ Puzzling Forest/Assets/Scripts/FoxHole.cs
@@ -7,76 +7,80 @@ public class FoxHole : MonoBehaviour
 
     [SerializeField]
     private FoxHole destinationFoxhole = null;
-    private bool isActivated = true;
+
+    private WarningMessagesController warnController = null;
+    private static string coveredWarning = "The exit of this Fox Hole is covered by something!";
 
     /*
      * Pseudocode - Teleport player from foxhole A to B
      *      
      *      1. Check if player has walked onto foxhole
-     *      2. if 1. = true, change player position = destinationFoxhole's position + Vector3.up
-     *      
+     *      2. if 1. = true, tell paired foxhole to warp this fox to itself
+     *      3. Other foxhole checks to make sure it's clear
+     *      4. If it is clear, warp this fox to itself + up
      *      
      *      - Translate player's position to destination foxhole position
      *      - 
      * 
      */
 
-    public bool GetIsActivated()
+    private void Start()
     {
-        return isActivated;
-    }
-    public void SetActivated(bool activate)
-    {
-        this.isActivated = activate;
+        warnController = GameObject.Find("UI Canvas").GetComponent<WarningMessagesController>();
     }
 
-    private void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        //CheckIfPlayerWalkedOntoFoxhole();
-
-    }
-
-
-    private void OnTriggerStay(Collider other)
-    {
-
-        if (other.gameObject.tag.Equals("Player")       //If This is the player...
-            && destinationFoxhole != null               //..AND the other foxhole is NOT null...
-            && destinationFoxhole.GetIsActivated())     //..AND if the other foxhole is active
+        if (other.gameObject.tag.Equals("Player") && destinationFoxhole != null) 
         {
-            Debug.Log("Player Entered " + this.gameObject.name);
             GameObject player = other.gameObject;
             TurnBasedCharacter playerTBC = player.GetComponent<TurnBasedCharacter>();
 
-            if (!playerTBC.GetIsMoving()) // Check if the player has stopped moving before teleporting
-            {
-                destinationFoxhole.WarpToMe(player);
-                this.isActivated = false;
-            }
+            StartCoroutine(WarpToMeAfterMoving(player, playerTBC));
         }
     }
-
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag.Equals("Player") && destinationFoxhole != null)
-        {
-            destinationFoxhole.SetActivated(true);
-        }
+
     }
 
+    private bool CheckIfUncovered()
+    {
+        Collider[] potentialCoverings = Physics.OverlapSphere(this.transform.position + Vector3.up, .1f);
+
+        return !(potentialCoverings.Length > 0);
+    }
 
     //Called by the other paired foxhole to warp the target object to this tile
-    public void WarpToMe(GameObject objectToWarp)
+    private void WarpToMe(GameObject objectToWarp)
     {
-        if (objectToWarp.tag.Equals("Player") && isActivated)
+        if (CheckIfUncovered())
         {
             Vector3 destinationPosition = this.gameObject.transform.position + Vector3.up;
             
             objectToWarp.GetComponent<TurnBasedCharacter>().SetTargetMoveToPosition(destinationPosition);
             objectToWarp.transform.position = destinationPosition;
-            
-            isActivated = false;
+
+            Debug.LogFormat("Warping {0} to {1}", objectToWarp.name, this.name);
+        }
+        else
+        {
+            warnController.Warn(coveredWarning);
+        }
+    }
+
+    public IEnumerator WarpToMeAfterMoving(GameObject player, TurnBasedCharacter TBC)
+    {
+        //This check makes sure the fox doesn't start TPing back and forth, only the time when
+        // it actually walks onto the foxhole will trigger anything.
+        if (TBC.GetIsMoving())
+        {
+            while (TBC.GetIsMoving())
+            {
+                yield return null;
+            }
+            destinationFoxhole.WarpToMe(player);
         }
     }
 }
