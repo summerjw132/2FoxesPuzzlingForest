d109f69aa191804ca0f0c1e9266be1062d84aa67
Garrison Lee <gmlee970@gmail.com>
2021-02-24 17:26:15 -0700

    FoxHole New Version
     - The player must now interact to trigger a foxhole
        - 'F' keybind
        - Button appears above fox head (size and position relative to screen size) that can also be pressed to interact
    
     - The button is always displayed (when a fox is on a foxhole) but a warning message is displayed if they try to interact and the other hole is blocked.

--- Puzzling Forest/Assets/Scripts/FoxHole.cs
+++ Puzzling Forest/Assets/Scripts/FoxHole.cs
@@ -8,8 +8,13 @@ public class FoxHole : MonoBehaviour
     [SerializeField]
     private FoxHole destinationFoxhole = null;
 
+    //stuff to warn the player via UI if the exit hole is blocked
     private WarningMessagesController warnController = null;
     private static string coveredWarning = "The exit of this Fox Hole is covered by something!";
+
+    //stuff to track and affect the game object on top of the foxhole
+    private GameObject standingOnMe = null;
+    private TurnBasedCharacter playerTBC = null;
 
     /*
      * Pseudocode - Teleport player from foxhole A to B
@@ -29,22 +34,62 @@ public class FoxHole : MonoBehaviour
         warnController = GameObject.Find("UI Canvas").GetComponent<WarningMessagesController>();
     }
 
+    // Update the vars tracking the GameObject that is on top of this foxhole
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag.Equals("Player") && destinationFoxhole != null) 
+        standingOnMe = other.gameObject;
+        
+        if (standingOnMe.CompareTag("Player"))
         {
-            GameObject player = other.gameObject;
-            TurnBasedCharacter playerTBC = player.GetComponent<TurnBasedCharacter>();
+            playerTBC = standingOnMe.GetComponent<TurnBasedCharacter>();
 
-            StartCoroutine(WarpToMeAfterMoving(player, playerTBC));
+            if (destinationFoxhole)
+                playerTBC.ShowFoxholeButton(true, this);
         }
     }
 
+    // Update the vars to reflect that this hole is no longer covered
     private void OnTriggerExit(Collider other)
     {
-
+        playerTBC.ShowFoxholeButton(false, null);
+        standingOnMe = null;
+        playerTBC = null;
     }
 
+    // Now using Update to check for key input. Blocked behind conditionals to make sure a player is on top
+    //  before allowing input
+    private void Update()
+    {
+        //There's something on this foxhole
+        if (standingOnMe)
+        {
+            //There's a player standing on this foxhole
+            if (standingOnMe.CompareTag("Player"))
+            {
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    InitiateWarp();
+                }
+            }
+        }
+    }
+
+    //public method for starting a warp. Includes checks to make sure the warp is valid and warns if not
+    public void InitiateWarp()
+    {
+        //If we get here we've already checked that standingOnMe is a player
+        if (destinationFoxhole != null)
+        {
+            if (!playerTBC.GetIsMoving())
+            {
+                playerTBC.WriteFoxholeToUndoStack();
+                playerTBC.IncrementMoveCounter();
+                destinationFoxhole.WarpToMe(standingOnMe);
+            }
+        }
+    }
+
+    //a way to return whether or not the foxhole is blocked before warping to it
     private bool CheckIfUncovered()
     {
         Collider[] potentialCoverings = Physics.OverlapSphere(this.transform.position + Vector3.up, .1f);
@@ -67,20 +112,6 @@ public class FoxHole : MonoBehaviour
         else
         {
             warnController.Warn(coveredWarning);
-        }
-    }
-
-    public IEnumerator WarpToMeAfterMoving(GameObject player, TurnBasedCharacter TBC)
-    {
-        //This check makes sure the fox doesn't start TPing back and forth, only the time when
-        // it actually walks onto the foxhole will trigger anything.
-        if (TBC.GetIsMoving())
-        {
-            while (TBC.GetIsMoving())
-            {
-                yield return null;
-            }
-            destinationFoxhole.WarpToMe(player);
         }
     }
 }
