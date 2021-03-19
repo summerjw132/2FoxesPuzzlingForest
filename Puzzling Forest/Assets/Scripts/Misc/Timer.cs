24451f3dca21fd7e42372f33a3ce9796eaac87c5
Garrison Lee <gmlee970@gmail.com>
2021-03-19 10:39:55 -0600

    Changed Timer Text
    Timer now returns string like "minutes : seconds" instead of just seconds.

--- Puzzling Forest/Assets/Scripts/Misc/Timer.cs
+++ Puzzling Forest/Assets/Scripts/Misc/Timer.cs
@@ -8,31 +8,95 @@ public class Timer : MonoBehaviour
 {
     [SerializeField]
     private GameObject timeUI = null;
+    private Text timeText = null;
 
-    private int timer;
     [HideInInspector]
     public bool isLevelComplete;
 
     private float timeAtStartOfThisLevel;
 
+    private int minutes = 0;
+    private int seconds = 0;
     private float curTime;
+    private string displayTime = "00:00";
 
-    public int totaltime {get{return timer;} set{}}
+    private bool paused = false;
+    private WaitForSeconds oneSecond;
+
+    public int totaltime {get{return seconds;} set{}}
+
+    void Awake()
+    {
+        timeText = timeUI.GetComponent<Text>();
+        oneSecond = new WaitForSeconds(1f);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         timeAtStartOfThisLevel = Time.time;
+        StartCoroutine(KeepTime());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isLevelComplete)
+
+    }
+
+    private IEnumerator KeepTime()
+    {
+        while (true)
         {
-            curTime = Time.time - timeAtStartOfThisLevel;
-            timer = (int)curTime;
+            if (paused)
+            {
+
+            }
+
+            else
+            {
+                curTime = Time.time - timeAtStartOfThisLevel + 55;
+
+                minutes = 0;
+                for (int i = (int)curTime; i >= 60; i = i - 60)
+                {
+                    minutes++;
+                }
+                seconds = (int)(curTime - (minutes * 60f));
+                if (minutes > 9)
+                {
+                    if (seconds > 9)
+                        displayTime = minutes.ToString() + ":" + seconds.ToString();
+                    else
+                        displayTime = minutes.ToString() + ":0" + seconds.ToString();
+                }
+                else
+                {
+                    if (seconds > 9)
+                        displayTime = "0" + minutes.ToString() + ":" + seconds.ToString();
+                    else
+                        displayTime = "0" + minutes.ToString() + ":0" + seconds.ToString();
+                }
+            }
+
+            timeText.text = displayTime;
+            yield return oneSecond;
         }
-        timeUI.GetComponent<Text>().text = timer.ToString();
+    }
+
+    public void ResetTimer()
+    {
+        timeAtStartOfThisLevel = Time.time;
+        minutes = 0;
+    }
+
+    public void TogglePause()
+    {
+        paused = !paused;
+    }
+
+    public void SetPause(bool val)
+    {
+        paused = val;
     }
 }
