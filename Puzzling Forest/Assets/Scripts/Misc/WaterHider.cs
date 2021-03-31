9fbd7c8cbbf0df5247a9552683d6669a06b24fca
Garrison Lee <gmlee970@gmail.com>
2021-03-31 17:36:32 -0600

    Water_Day_1
    -Continuous water and effect.
    -Water is hidden when a block is overlapping.
    -Wave effect.
    -Foam effect NOT implemented in this commit, coming tomorrow (hopefully)

---
+++ Puzzling Forest/Assets/Scripts/Misc/WaterHider.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterHider : MonoBehaviour
+{
+    private GameObject water;
+
+    void Awake()
+    {
+        water = this.transform.parent.transform.GetChild(0).gameObject;
+        Debug.LogFormat("Me: {0}\tMy water: {1}", this.transform.parent.name, water.name);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter()
+    {
+        water.SetActive(false);
+    }
+
+    void OnTriggerExit()
+    {
+        water.SetActive(true);
+    }
+}
